650580598965add7010d304939d9ed98e218a730
Matthew Jones <Matthew.J.Jones@stfc.ac.uk>
2020-04-07 08:55:36 +0100

    changed metrics function generator to use eval

--- jasmin_metrics/metrics.py
+++ jasmin_metrics/metrics.py
@@ -14,9 +14,9 @@ import time
 
 
 class MetricsView(object):
-    def __init__(self, req_metrics_file):
+    def __init__(self, metrics_group, req_metrics_file='jasmin_metrics/metrics.ini'):
         self.collector = pc.CollectorRegistry()
-        self.req_metrics = self.parse_metrics_config(req_metrics_file)
+        self.metrics_group = metrics_group
         lotus = LotusMetrics()
         storage = StorageMetrics()
         mc = MCMetrics()
@@ -25,176 +25,141 @@ class MetricsView(object):
 
         self.req_metrics = self.parse_metrics_config(req_metrics_file)
 
-        # define dictionary of calculation functions
-        self.met_funcs = {
-            'storage_total': storage.get_storage_total,
-            'storage_used': storage.get_storage_used,
-            'storage_com': storage.get_storage_com,
-            'storage_pfs_total': storage.get_storage_pfs_total,
-            'storage_pfs_used': storage.get_storage_pfs_used,
-            'storage_sof_total': storage.get_storage_sof_total,
-            'storage_sof_used': storage.get_storage_sof_used,
-            'storage_el_total': storage.get_storage_el_total,
-            'storage_el_used': storage.get_storage_el_used,
-            'lotus_cores_count': lotus.get_lotus_cores_count,
-            'lotus_hosts_count': lotus.get_lotus_hosts_count,
-            'lotus_mem_total': lotus.get_lotus_mem_total,
-            'lotus_tbmonth_in': lotus.get_lotus_tbmonth_in,
-            'lotus_tbmonth_out': lotus.get_lotus_tbmonth_out,
-            'lotus_network_in': lotus.get_lotus_network_in,
-            'lotus_network_out': lotus.get_lotus_network_out,
-            'lotus_core_hours_day': lotus.get_lotus_core_hours_day,
-            'lotus_core_hours_3day': lotus.get_lotus_core_hours_3day,
-            'lotus_core_hours_week': lotus.get_lotus_core_hours_week,
-            'lotus_core_hours_month': lotus.get_lotus_core_hours_month,
-            'lotus_core_hours_avg_day': lotus.get_lotus_core_hours_avg_day,
-            'lotus_core_hours_avg_3day': lotus.get_lotus_core_hours_avg_3day,
-            'lotus_core_hours_avg_week': lotus.get_lotus_core_hours_avg_week,
-            'lotus_core_hours_avg_month': lotus.get_lotus_core_hours_avg_month,
-            'lotus_util_day': lotus.get_lotus_util_day,
-            'lotus_util_3day': lotus.get_lotus_util_3day,
-            'lotus_util_week': lotus.get_lotus_util_week,
-            'lotus_util_month': lotus.get_lotus_util_month,
-            'lotus_job_proc_min_day': lotus.get_lotus_job_proc_min_day,
-            'lotus_job_proc_min_3day': lotus.get_lotus_job_proc_min_3day,
-            'lotus_job_proc_min_week': lotus.get_lotus_job_proc_min_week,
-            'lotus_job_proc_min_month': lotus.get_lotus_job_proc_min_month,
-            'lotus_job_proc_avg_day': lotus.get_lotus_job_proc_avg_day,
-            'lotus_job_proc_avg_3day': lotus.get_lotus_job_proc_avg_3day,
-            'lotus_job_proc_avg_week': lotus.get_lotus_job_proc_avg_week,
-            'lotus_job_proc_avg_month': lotus.get_lotus_job_proc_avg_month,
-            'lotus_job_proc_max_day': lotus.get_lotus_job_proc_max_day,
-            'lotus_job_proc_max_3day': lotus.get_lotus_job_proc_max_3day,
-            'lotus_job_proc_max_week': lotus.get_lotus_job_proc_max_week,
-            'lotus_job_proc_max_month': lotus.get_lotus_job_proc_max_month,
-            'lotus_job_count_finished_day': lotus.get_lotus_job_count_finished_day,
-            'lotus_job_count_finished_3day': lotus.get_lotus_job_count_finished_3day,
-            'lotus_job_count_finished_week': lotus.get_lotus_job_count_finished_week,
-            'lotus_job_count_finished_month': lotus.get_lotus_job_count_finished_month,
-            'lotus_job_count_started_day': lotus.get_lotus_job_count_started_day,
-            'lotus_job_count_started_3day': lotus.get_lotus_job_count_started_3day,
-            'lotus_job_count_started_week': lotus.get_lotus_job_count_started_week,
-            'lotus_job_count_started_month': lotus.get_lotus_job_count_started_month,
-            'lotus_job_count_submitted_day': lotus.get_lotus_job_count_submitted_day,
-            'lotus_job_count_submitted_3day': lotus.get_lotus_job_count_submitted_3day,
-            'lotus_job_count_submitted_week': lotus.get_lotus_job_count_submitted_week,
-            'lotus_job_count_submitted_month': lotus.get_lotus_job_count_submitted_month,
-            'lotus_job_count_running_day': lotus.get_lotus_job_count_running_day,
-            'lotus_job_count_running_3day': lotus.get_lotus_job_count_running_3day,
-            'lotus_job_count_running_week': lotus.get_lotus_job_count_running_week,
-            'lotus_job_count_running_month': lotus.get_lotus_job_count_running_month,
-            'lotus_expansion_factor': lotus.get_lotus_expansion_factor,
-            'lotus_wait_dur_tot': lotus.get_lotus_wait_dur_tot,
-            'lotus_wait_dur_avg': lotus.get_lotus_wait_dur_avg,
-            'lotus_wall_dur_tot': lotus.get_lotus_wall_dur_tot,
-            'lotus_wall_dur_avg': lotus.get_lotus_wall_dur_avg,
-            'openstack_vms_count': mc.get_openstack_vms_count,
-            'openstack_vms_cpus_quota': mc.get_openstack_vms_cpus_quota,
-            'openstack_vms_cpus_used': mc.get_openstack_vms_cpus_used,
-            'openstack_vms_ram_quota': mc.get_openstack_vms_ram_quota,
-            'openstack_vms_ram_used': mc.get_openstack_vms_ram_used,
-            'openstack_vms_storage_quota': mc.get_openstack_vms_storage_quota,
-            'openstack_vms_storage_used': mc.get_openstack_vms_storage_used,
-            'archive_ingest1_load_1min': arch.get_archive_ingest1_load_1min,
-            'archive_ingest1_mem_swap': arch.get_archive_ingest1_mem_swap,
-            'archive_ingest2_load_1min': arch.get_archive_ingest2_load_1min,
-            'archive_ingest2_mem_swap': arch.get_archive_ingest2_mem_swap,
-            'archive_ingest3_load_1min': arch.get_archive_ingest3_load_1min,
-            'archive_ingest3_mem_swap': arch.get_archive_ingest3_mem_swap,
-            'archive_ingest4_load_1min': arch.get_archive_ingest4_load_1min,
-            'archive_ingest4_mem_swap': arch.get_archive_ingest4_mem_swap,
-            'archive_ingest5_load_1min': arch.get_archive_ingest5_load_1min,
-            'archive_ingest5_mem_swap': arch.get_archive_ingest5_mem_swap,
-            'archive_rbq_load_1min': arch.get_archive_rbq_load_1min,
-            'archive_rbq_mem_swap': arch.get_archive_rbq_mem_swap,
-            'archive_deposit1_load_1min': arch.get_archive_deposit1_load_1min,
-            'archive_deposit1_mem_swap': arch.get_archive_deposit1_mem_swap,
-            'archive_deposit2_load_1min': arch.get_archive_deposit2_load_1min,
-            'archive_deposit2_mem_swap': arch.get_archive_deposit2_mem_swap,
-            'archive_deposit3_load_1min': arch.get_archive_deposit3_load_1min,
-            'archive_deposit3_mem_swap': arch.get_archive_deposit3_mem_swap,
-            'archive_deposit4_load_1min': arch.get_archive_deposit4_load_1min,
-            'archive_deposit4_mem_swap': arch.get_archive_deposit4_mem_swap,
-            'archive_deposit5_load_1min': arch.get_archive_deposit5_load_1min,
-            'archive_deposit5_mem_swap': arch.get_archive_deposit5_mem_swap,
-            'archive_ingest1_up': arch.get_archive_ingest1_up,
-            'archive_ingest2_up': arch.get_archive_ingest2_up,
-            'archive_ingest3_up': arch.get_archive_ingest3_up,
-            'archive_ingest4_up': arch.get_archive_ingest4_up,
-            'archive_ingest5_up': arch.get_archive_ingest5_up,
-            'archive_rbq_up': arch.get_archive_rbq_up,
-            'archive_deposit1_up': arch.get_archive_deposit1_up,
-            'archive_deposit2_up': arch.get_archive_deposit2_up,
-            'archive_deposit3_up': arch.get_archive_deposit3_up,
-            'archive_deposit4_up': arch.get_archive_deposit4_up,
-            'archive_deposit5_up': arch.get_archive_deposit5_up,
-            #'users_jasmin': users.get_users_jasmin,
-            #'users_jasmin_login_active_today': users.get_users_jasmin_login_active_today,
-            #'users_jasmin_login_new_today': users.get_users_jasmin_login_new_today,
-            #'users_jasmin_login_new_week': users.get_users_jasmin_login_new_week,
-            #'users_jasmin_login_new_quarter': users.get_users_jasmin_login_new_quarter,
-}
+        ## define dictionary of calculation functions
+        #self.met_funcs = {
+            #'storage_total': storage.get_storage_total,
+            #'storage_used': storage.get_storage_used,
+            #'storage_com': storage.get_storage_com,
+            #'storage_pfs_total': storage.get_storage_pfs_total,
+            #'storage_pfs_used': storage.get_storage_pfs_used,
+            #'storage_sof_total': storage.get_storage_sof_total,
+            #'storage_sof_used': storage.get_storage_sof_used,
+            #'storage_el_total': storage.get_storage_el_total,
+            #'storage_el_used': storage.get_storage_el_used,
+            #'lotus_cores_count': lotus.get_lotus_cores_count,
+            #'lotus_hosts_count': lotus.get_lotus_hosts_count,
+            #'lotus_mem_total': lotus.get_lotus_mem_total,
+            #'lotus_tbmonth_in': lotus.get_lotus_tbmonth_in,
+            #'lotus_tbmonth_out': lotus.get_lotus_tbmonth_out,
+            #'lotus_network_in': lotus.get_lotus_network_in,
+            #'lotus_network_out': lotus.get_lotus_network_out,
+            #'lotus_core_hours_day': lotus.get_lotus_core_hours_day,
+            #'lotus_core_hours_3day': lotus.get_lotus_core_hours_3day,
+            #'lotus_core_hours_week': lotus.get_lotus_core_hours_week,
+            #'lotus_core_hours_month': lotus.get_lotus_core_hours_month,
+            #'lotus_core_hours_avg_day': lotus.get_lotus_core_hours_avg_day,
+            #'lotus_core_hours_avg_3day': lotus.get_lotus_core_hours_avg_3day,
+            #'lotus_core_hours_avg_week': lotus.get_lotus_core_hours_avg_week,
+            #'lotus_core_hours_avg_month': lotus.get_lotus_core_hours_avg_month,
+            #'lotus_util_day': lotus.get_lotus_util_day,
+            #'lotus_util_3day': lotus.get_lotus_util_3day,
+            #'lotus_util_week': lotus.get_lotus_util_week,
+            #'lotus_util_month': lotus.get_lotus_util_month,
+            #'lotus_job_proc_min_day': lotus.get_lotus_job_proc_min_day,
+            #'lotus_job_proc_min_3day': lotus.get_lotus_job_proc_min_3day,
+            #'lotus_job_proc_min_week': lotus.get_lotus_job_proc_min_week,
+            #'lotus_job_proc_min_month': lotus.get_lotus_job_proc_min_month,
+            #'lotus_job_proc_avg_day': lotus.get_lotus_job_proc_avg_day,
+            #'lotus_job_proc_avg_3day': lotus.get_lotus_job_proc_avg_3day,
+            #'lotus_job_proc_avg_week': lotus.get_lotus_job_proc_avg_week,
+            #'lotus_job_proc_avg_month': lotus.get_lotus_job_proc_avg_month,
+            #'lotus_job_proc_max_day': lotus.get_lotus_job_proc_max_day,
+            #'lotus_job_proc_max_3day': lotus.get_lotus_job_proc_max_3day,
+            #'lotus_job_proc_max_week': lotus.get_lotus_job_proc_max_week,
+            #'lotus_job_proc_max_month': lotus.get_lotus_job_proc_max_month,
+            #'lotus_job_count_finished_day': lotus.get_lotus_job_count_finished_day,
+            #'lotus_job_count_finished_3day': lotus.get_lotus_job_count_finished_3day,
+            #'lotus_job_count_finished_week': lotus.get_lotus_job_count_finished_week,
+            #'lotus_job_count_finished_month': lotus.get_lotus_job_count_finished_month,
+            #'lotus_job_count_started_day': lotus.get_lotus_job_count_started_day,
+            #'lotus_job_count_started_3day': lotus.get_lotus_job_count_started_3day,
+            #'lotus_job_count_started_week': lotus.get_lotus_job_count_started_week,
+            #'lotus_job_count_started_month': lotus.get_lotus_job_count_started_month,
+            #'lotus_job_count_submitted_day': lotus.get_lotus_job_count_submitted_day,
+            #'lotus_job_count_submitted_3day': lotus.get_lotus_job_count_submitted_3day,
+            #'lotus_job_count_submitted_week': lotus.get_lotus_job_count_submitted_week,
+            #'lotus_job_count_submitted_month': lotus.get_lotus_job_count_submitted_month,
+            #'lotus_job_count_running_day': lotus.get_lotus_job_count_running_day,
+            #'lotus_job_count_running_3day': lotus.get_lotus_job_count_running_3day,
+            #'lotus_job_count_running_week': lotus.get_lotus_job_count_running_week,
+            #'lotus_job_count_running_month': lotus.get_lotus_job_count_running_month,
+            #'lotus_expansion_factor': lotus.get_lotus_expansion_factor,
+            #'lotus_wait_dur_tot': lotus.get_lotus_wait_dur_tot,
+            #'lotus_wait_dur_avg': lotus.get_lotus_wait_dur_avg,
+            #'lotus_wall_dur_tot': lotus.get_lotus_wall_dur_tot,
+            #'lotus_wall_dur_avg': lotus.get_lotus_wall_dur_avg,
+            #'openstack_vms_count': mc.get_openstack_vms_count,
+            #'openstack_vms_cpus_quota': mc.get_openstack_vms_cpus_quota,
+            #'openstack_vms_cpus_used': mc.get_openstack_vms_cpus_used,
+            #'openstack_vms_ram_quota': mc.get_openstack_vms_ram_quota,
+            #'openstack_vms_ram_used': mc.get_openstack_vms_ram_used,
+            #'openstack_vms_storage_quota': mc.get_openstack_vms_storage_quota,
+            #'openstack_vms_storage_used': mc.get_openstack_vms_storage_used,
+            #'archive_ingest1_load_1min': arch.get_archive_ingest1_load_1min,
+            #'archive_ingest1_mem_swap': arch.get_archive_ingest1_mem_swap,
+            #'archive_ingest2_load_1min': arch.get_archive_ingest2_load_1min,
+            #'archive_ingest2_mem_swap': arch.get_archive_ingest2_mem_swap,
+            #'archive_ingest3_load_1min': arch.get_archive_ingest3_load_1min,
+            #'archive_ingest3_mem_swap': arch.get_archive_ingest3_mem_swap,
+            #'archive_ingest4_load_1min': arch.get_archive_ingest4_load_1min,
+            #'archive_ingest4_mem_swap': arch.get_archive_ingest4_mem_swap,
+            #'archive_ingest5_load_1min': arch.get_archive_ingest5_load_1min,
+            #'archive_ingest5_mem_swap': arch.get_archive_ingest5_mem_swap,
+            #'archive_rbq_load_1min': arch.get_archive_rbq_load_1min,
+            #'archive_rbq_mem_swap': arch.get_archive_rbq_mem_swap,
+            #'archive_deposit1_load_1min': arch.get_archive_deposit1_load_1min,
+            #'archive_deposit1_mem_swap': arch.get_archive_deposit1_mem_swap,
+            #'archive_deposit2_load_1min': arch.get_archive_deposit2_load_1min,
+            #'archive_deposit2_mem_swap': arch.get_archive_deposit2_mem_swap,
+            #'archive_deposit3_load_1min': arch.get_archive_deposit3_load_1min,
+            #'archive_deposit3_mem_swap': arch.get_archive_deposit3_mem_swap,
+            #'archive_deposit4_load_1min': arch.get_archive_deposit4_load_1min,
+            #'archive_deposit4_mem_swap': arch.get_archive_deposit4_mem_swap,
+            #'archive_deposit5_load_1min': arch.get_archive_deposit5_load_1min,
+            #'archive_deposit5_mem_swap': arch.get_archive_deposit5_mem_swap,
+            #'archive_ingest1_up': arch.get_archive_ingest1_up,
+            #'archive_ingest2_up': arch.get_archive_ingest2_up,
+            #'archive_ingest3_up': arch.get_archive_ingest3_up,
+            #'archive_ingest4_up': arch.get_archive_ingest4_up,
+            #'archive_ingest5_up': arch.get_archive_ingest5_up,
+            #'archive_rbq_up': arch.get_archive_rbq_up,
+            #'archive_deposit1_up': arch.get_archive_deposit1_up,
+            #'archive_deposit2_up': arch.get_archive_deposit2_up,
+            #'archive_deposit3_up': arch.get_archive_deposit3_up,
+            #'archive_deposit4_up': arch.get_archive_deposit4_up,
+            #'archive_deposit5_up': arch.get_archive_deposit5_up,
+            ##'users_jasmin': users.get_users_jasmin,
+            ##'users_jasmin_login_active_today': users.get_users_jasmin_login_active_today,
+            ##'users_jasmin_login_new_today': users.get_users_jasmin_login_new_today,
+            ##'users_jasmin_login_new_week': users.get_users_jasmin_login_new_week,
+            ##'users_jasmin_login_new_quarter': users.get_users_jasmin_login_new_quarter,
+            #}
 
-<<<<<<< HEAD:jasmin_metrics/metrics.py
         self.service_status_list = {}
         # gauges
-        for m in self.req_metrics['gauge']:
+        self.met_funcs = {}
+        for m in self.req_metrics:
+            if 'lotus_' in m: 
+                m_func = eval('lotus.get_{}'.format(m))
+            elif 'archive_' in m:
+                m_func = eval('arch.get_{}'.format(m))
+            elif 'storage_' in m:
+                m_func = eval('storage.get_{}'.format(m))
+            self.met_funcs[m] = m_func
             gauge = pc.Gauge(m, m, registry=self.collector)
             self.service_status_list[m] = (gauge)
 
 
     def parse_metrics_config(self, fname):
-=======
-
-
-    def parse_metrics_config(self, fname='./metrics.ini'):
->>>>>>> master:jasmin_metrics/metrics.py
-        req_metrics = {}
         config = configparser.ConfigParser()
         config.read(fname)
-        req_metrics['gauge'] = config.get('Metrics','gauge').split('\n')
+        req_metrics = config.get('Metrics',self.metrics_group).split('\n')
 
         return req_metrics
 
-<<<<<<< HEAD:jasmin_metrics/metrics.py
     def create_view(self):
-        for m in self.req_metrics['gauge']:
+        for m in self.req_metrics:
             self.service_status_list[m].set(self.met_funcs[m]())
 
         return pc.generate_latest(registry=self.collector)
-=======
-    def gen_metrics(self):
-        collector = pc.CollectorRegistry()
-        service_status_list = {}
-        # gauges
-        for m in self.req_metrics['gauge']:
-            gauge = pc.Gauge(m, m, registry=collector)
-            service_status_list[m] = (gauge)
 
-        return service_status_list
-
-    def create_view(self):
-        service_status_list = gen_metrics()
-
-        for m in self.req_metrics['gauge']:
-            service_status_list[m].set(self.met_funcs[m]())
-
-        return pc.generate_latest(registry=self.collector)
-
-
-    def create_metrics_view(self):
-        return self.create_view('./metrics.ini', '/metrics/', 'metrics')
-    
-    def create_daily_metrics_view(self):
-        return self.create_view('./daily_metrics.ini','/daily_metrics/','daily metrics')
-
-    def create_weekly_metrics_view(self):
-        return self.create_view('./weekly_metrics.ini','/weekly_metrics/','weekly metrics')
-
-    def create_monthly_metrics_view(self):
-        return self.create_view('./monthly_metrics.ini','/monthly_metrics/','monthly metrics')
-
-    def create_arch_metrics_view(self):
-        return self.create_view('./arch_metrics.ini','/arch_metrics/','archive metrics')
-
->>>>>>> master:jasmin_metrics/metrics.py
--- jasmin_metrics/views.py
+++ jasmin_metrics/views.py
@@ -6,23 +6,23 @@ def index(request):
     return HttpResponse('Metrics Index')
 
 def arch_metrics(request):
-    am = MetricsView('jasmin_metrics/arch_metrics.ini')
+    am = MetricsView('archive')
     return HttpResponse(am.create_view(), content_type='text/plain')
 
 def freq_metrics(request):
-    fm = MetricsView('jasmin_metrics/metrics.ini')
+    fm = MetricsView('freq')
     return HttpResponse(fm.create_view(), content_type='text/plain')
 
 def daily_metrics(request):
-    dm = MetricsView('jasmin_metrics/daily_metrics.ini')
+    dm = MetricsView('daily')
     return HttpResponse(dm.create_view(), content_type='text/plain')
 
 def weekly_metrics(request):
-    wm = MetricsView('jasmin_metrics/weekly_metrics.ini')
+    wm = MetricsView('weekly')
     return HttpResponse(wm.create_view(), content_type='text/plain')
 
 def monthly_metrics(request):
-    mm = MetricsView('jasmin_metrics/monthly_metrics.ini')
+    mm = MetricsView('monthly')
     return HttpResponse(mm.create_view(), content_type='text/plain')
 
 
